fd7f32c9844fc90883e9a39e86ce56b3437914c2
45inertia <114481820+45inertia@users.noreply.github.com>
2024-05-18 16:12:55 +0100

    Added empty functions and empty "listItem" class. Added menu display for main()

---
+++ ToDoList_v2.cpp
@@ -0,0 +1,40 @@
+// The purpose of this program is to iterate of version 1 of this project. 
+// The aims are as follows:
+// 1) Create an item class that has: id, title, description, days (how many days until deadline).
+// 2) Split previous functionality into functions. 
+
+#include <iostream>
+
+int main() {
+	int endCheck{ 1 };
+	int menuNum{};
+
+	while (endCheck == 1) {
+		std::cout << "\n(1) View List\n";
+		std::cout << "(2) Add items to list\n";
+		std::cout << "(3) Remove item from list\n";
+		std::cout << "(4) Exit Program\n";
+		std::cout << "\nPlease select your option: ";
+
+	}
+
+
+
+	return 0;
+}
+
+int displayList() {
+
+}
+
+int addItemToList() {
+
+}
+
+int removeItemFromList() {
+
+}
+
+class listItem {
+
+};
